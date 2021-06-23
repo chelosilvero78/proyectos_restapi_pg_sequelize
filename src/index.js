bdcb23b6ce5e0f345187008f6b4ea4733a916368
chelosilvero78 <marcesilvero@gmail.com>
2021-06-23 14:24:57 -0400

    agregado comentario al index.js sobre como optener port desde express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import app from './app';
 import '@babel/polyfill';
 
+/*
+//COMO OPTENER EL PUERTO DESDE EXPRESS
+const aplicacion=await app.listen(4000);
+    console.log('Server on port', aplicacion.address().port);
+
+*/
+
 async function main () {
     await app.listen(4000);
     console.log('Server on port 4000');
